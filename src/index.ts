3845d0cfe5d468870e39986f573315f02d055c31
mukharomdev <mukharomdev@gmail.com>
2025-03-01 07:42:08 +0700

    update src

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,45 @@
-// src/index.ts
-import {express}  from "./libraries";
+// // src/index.ts
+import {express,dotenv}  from "@/libraries";
+import {getPostSlugs,getPostBySlug,markdownToHtml} from '@/utils'
 
-console.log(express);
+/*
+ * Load up and parse configuration details from
+ * the `.env` file to the `process.env`
+ * object of Node.js
+ */
+dotenv.config();
 
-// /*
-//  * Load up and parse configuration details from
-//  * the `.env` file to the `process.env`
-//  * object of Node.js
-//  */
-// dotenv.config();
+/*
+ * Create an Express application and get the
+ * value of the PORT environment variable
+ * from the `process.env`
+ */
+const app = express();
+const port = process.env.PORT || 3000;
 
-// /*
-//  * Create an Express application and get the
-//  * value of the PORT environment variable
-//  * from the `process.env`
-//  */
-// const app: Express = express();
-// const port = process.env.PORT || 3000;
+/* Define a route for the root path ("/")
+ using the HTTP GET method */
+ app.get("/", (req, res) => {
+  const message = 
+  "<h1>Express + PostGresSql + Sequleize TypeScript Server</h1>"
+  // const posts = getPostSlugs()
+  // const content = getPostBySlug(posts[0]).content
+  // const content1 = content.replace(/(^|\W)`([^`\n]+)`(\W|$)/g, '$1<code>$2</code>$3')  
+  // const html  = markdownToHtml(content1)
+  // .then(n=>res.send(n))
+         
 
-// /* Define a route for the root path ("/")
-//  using the HTTP GET method */
-// app.get("/", (req: Request, res: Response) => {
 
-//   res.send("Express + PostGresSql + Sequleize TypeScript Server");
-// });
+  
+    res.send(message);
+});
 
-// /* Start the Express app and listen
-//  for incoming requests on the specified port */
-// app.listen(port, () => {
-//   console.log(`[server]: Server is running at http://localhost:${port}`);
-// });
+/* Start the Express app and listen
+ for incoming requests on the specified port */
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+
+
+
--- src/libraries/index.ts
+++ src/libraries/index.ts
@@ -1,5 +1,5 @@
 /* LIBRARIES */
-
+/*Server package */
 import express	 from "express";
 import postgress from 'pg';
 import cors		 from 'cors'
@@ -7,12 +7,27 @@ import bodyparser from 'body-parser'
 import Sequelize  from 'sequelize'
 import dotenv     from 'dotenv'
 
+/* content package */
+//import { remark } from "remark";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm"; // Untuk dukungan tabel
+import remarkHtml from "remark-html";
+import matter from "gray-matter";
+
+
+
 export {
 	express,
 	postgress,
 	cors,
 	bodyparser,
 	Sequelize,
-	dotenv
+	dotenv,
+	unified,
+	remarkGfm,
+	remarkParse,
+	remarkHtml,
+	matter
 }
 
